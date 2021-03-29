cef41b531637a670b921e4fc6a316ecd5541f088
wombat010 <sljivkov@gmail.com>
2021-03-29 19:46:43 +0200

    odradjen wpf

---
+++ Hospital/Hospital/RoomOptions.xaml.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+using System.Windows;
+
+
+namespace Hospital
+{
+
+    public partial class RoomOptions : Window
+    {
+        public RoomOptions()
+        {
+            InitializeComponent();
+        }
+
+        private void createRooms(object sender, RoutedEventArgs e)
+        {
+            RoomForm room = new RoomForm();
+            room.Show();
+        }
+    }
+}
