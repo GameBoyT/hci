6bf33e0526b9275d6069424a51467ab15a96640d
gmb <gameboyt7@gmail.com>
2021-04-29 14:36:53 +0200

    Dodat referral u diagram

--- Hospital/Hospital/Model/Employee.cs
+++ Hospital/Hospital/Model/Employee.cs
@@ -18,13 +18,11 @@ namespace Model
         public int AnnualLeave { get; set; }
 
         public Room Room { get; set; }
-        public String Specialization { get; set; }
 
+        public String Specialization { get; set; }
 
         public System.Collections.Generic.List<Appointment> Appointment { get; set; }
 
         public System.Collections.Generic.List<Notification> Notification { get; set; }
-
-
     }
 }
