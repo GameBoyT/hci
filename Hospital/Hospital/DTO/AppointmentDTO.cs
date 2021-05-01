492fc2b08c5fe2be88bcb1311a22ceaa998be8be
nemanjamarkovic <nemanja_markovic1998@hotmail.com>
2021-05-01 11:07:09 +0200

    uput zavrsen, treba dodati ogranicenja

---
+++ Hospital/Hospital/DTO/AppointmentDTO.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace DTO
+{
+    public class AppointmentDTO
+    {
+        public Appointment Appointment { get; set; }
+        public String DoctorJMBG { get; set; }
+        public String DoctorFirstName { get; set; }
+        public String DoctorLastName { get; set; }
+      //  public String DoctorSpecialization { get; set; }
+        public String PatientJMBG { get; set; }
+        public String PatientFirstName { get; set; }
+        public String PatientLastName { get; set; }
+        public String RoomName { get; set; }
+
+        public AppointmentDTO(Appointment appointment, String doctorJmbg, string doctorFirstName, string doctorLastName)
+        {
+            Appointment = appointment;
+            DoctorJMBG = doctorJmbg;
+            DoctorFirstName = doctorFirstName;
+            DoctorLastName = doctorLastName;
+            
+
+        }
+
+        public AppointmentDTO (Appointment appointment, String patientJmbg, String patientFirstName, String patientLastName, String roomName)
+        {
+            Appointment = appointment;
+            PatientJMBG = patientJmbg;
+            PatientFirstName = patientFirstName;
+            PatientLastName = patientLastName;
+            RoomName = roomName;
+
+        }
+
+
+    }
+}
---
+++ Hospital/Hospital/DTO/ReferralDTO.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Model;
+
+namespace DTO
+{
+    public class ReferralDTO
+    {
+        public Employee Doctor { get; set; }
+        public String Description { get; set; }
+
+        public ReferralDTO(Employee doctor, String description)
+        {
+            this.Doctor = doctor;
+            this.Description = description;
+        }
+        
+    }
+}
