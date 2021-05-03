397992ab9ab5fe66120c8be4e15187e94f0d3a86
wombat010 <sljivkov@gmail.com>
2021-05-03 18:14:05 +0200

    zavrsen crud za lekove

--- Hospital/Hospital/View/Director/MedicineCrud.xaml.cs
+++ Hospital/Hospital/View/Director/MedicineCrud.xaml.cs
@@ -2,13 +2,8 @@
 using System.Collections.Generic;
 using System.Text;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Shapes;
+using Model;
+using Controller;
 
 namespace Hospital.View.Director
 {
@@ -17,9 +12,91 @@ namespace Hospital.View.Director
     /// </summary>
     public partial class MedicineCrud : Window
     {
+
+        private MedicineController medController = new MedicineController();
+        private int id; // pomaze oko update 
+                        // List<Room> rooms = new List<Room>();
+        List<Medicine> medsToShow = new List<Medicine>();
         public MedicineCrud()
         {
             InitializeComponent();
+            medsToShow = medController.GetAll();
+            medicineDataGrid.ItemsSource = medsToShow;
+        }
+
+
+        private Medicine CreateMed()
+        {
+            int id = medController.GenerateNewId();
+            string medicineName = name.Text;
+            string medicineDescription = description.Text;
+            medController.Save(medicineName, medicineDescription);
+            return new Medicine(id, medicineName, medicineDescription, " ");
+        }
+
+        private void New_Medicine_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Medicine med = CreateMed();
+            }
+            catch
+            {
+                MessageBox.Show("You have to fill in all input boxes!");
+            }
+        }
+        private void Delete_Med_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Medicine med = (Medicine)medicineDataGrid.SelectedItems[0];
+                medController.Delete(med.Id);
+            }
+            catch
+            {
+                MessageBox.Show("You have to select a room to delete!");
+            }
+        }
+
+        private void UpdateMedShow_Click(object sender, RoutedEventArgs e)
+        {
+            try
+            {
+                Medicine med = (Medicine)medicineDataGrid.SelectedItems[0];
+                id = med.Id;
+                createMedicine.Visibility = Visibility.Collapsed;
+                updateMedButton.Visibility = Visibility.Visible;
+                cancelupdateMedButton.Visibility = Visibility.Visible;
+                title.Content = "Edit medication";
+
+                name.Text = med.Name;
+                description.Text = med.Description;
+
+            }
+            catch
+            {
+                MessageBox.Show("You have to fill in all input boxes!");
+            }
+        }
+
+        private void Update_Med_Click(object sender, RoutedEventArgs e)
+        {
+            string medicineName = name.Text;
+            string medicineDescription = description.Text;
+            medController.Update(id, medicineName, 0, medicineDescription);
+            id = -1;
+        }
+
+        private void CancelupdateMedButton_Click(object sender, RoutedEventArgs e)
+        {
+            name.Text = "";
+            description.Text = "";
+
+
+            createMedicine.Visibility = Visibility.Visible;
+            updateMedButton.Visibility = Visibility.Collapsed;
+            cancelupdateMedButton.Visibility = Visibility.Collapsed;
+            title.Content = "Create new med";
         }
     }
 }
