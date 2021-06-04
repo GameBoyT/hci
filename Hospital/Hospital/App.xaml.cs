c971a458c15a616ac730ca83e42111cb3f791dd8
gmb <gameboyt7@gmail.com>
2021-06-04 23:32:51 +0200

    Izbaceni kontroleri iz app.cs

--- Hospital/Hospital/App.xaml.cs
+++ Hospital/Hospital/App.xaml.cs
@@ -8,19 +8,8 @@ namespace Hospital
 {
     public partial class App : Application
     {
-        private static readonly string _medicineFileLocation = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Data\\medicines.json";
-
-
-
-        private static readonly MedicineRepository medicineRepository = new MedicineRepository(_medicineFileLocation);
-
-
-
-        public readonly AppointmentController appointmentController = new AppointmentController();
-        public readonly PatientController patientController = new PatientController();
-        public readonly RoomController roomController = new RoomController();
-        public readonly StaticEquipmentController staticEquipmentController = new StaticEquipmentController();
-        public readonly MedicineController medicineController = new MedicineController(medicineRepository);
-        public readonly EmployeeController employeeController = new EmployeeController();
+        //private static readonly string _medicineFileLocation = Directory.GetParent(Environment.CurrentDirectory).Parent.Parent.FullName + "\\Data\\medicines.json";
+        //private static readonly MedicineRepository medicineRepository = new MedicineRepository(_medicineFileLocation);
+        //public readonly MedicineController medicineController = new MedicineController(medicineRepository);
     }
 }
