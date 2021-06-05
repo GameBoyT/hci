b29d55261d280a7b3f1ad3d5181e3375d81e734a
gmb <gameboyt7@gmail.com>
2021-06-05 21:57:44 +0200

    Dodat vm za medicin i alternativu i dodat konverter za njih

---
+++ Hospital/Hospital/VMConverters/MedicineConverter.cs
@@ -0,0 +1,63 @@
+﻿using Hospital.ViewModels.DTO;
+using Model;
+using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.Text;
+
+namespace Hospital.VMConverters
+{
+    public class MedicineConverter
+    {
+        public MedicineViewModel ConvertModelToViewModel(Medicine medicine)
+        {
+            MedicineViewModel medicineViewModel = new MedicineViewModel
+            {
+                Id = medicine.Id,
+                Name = medicine.Name,
+                Description = medicine.Description,
+                DoctorComment = medicine.DoctorComment,
+                Alternatives = ConvertCollectionToAlternativeViewModel(medicine.Alternatives),
+                Ingredients = new ObservableCollection<string>(medicine.Ingredients),
+                _Medicine = medicine
+            };
+
+            return medicineViewModel;
+        }
+
+        public ObservableCollection<MedicineViewModel> ConvertCollectionToViewModel(List<Medicine> medicines)
+        {
+            ObservableCollection<MedicineViewModel> vmMedicines = new ObservableCollection<MedicineViewModel>();
+            MedicineViewModel medicineViewModel;
+            foreach (Medicine medicine in medicines)
+            {
+                medicineViewModel = ConvertModelToViewModel(medicine);
+                vmMedicines.Add(medicineViewModel);
+            }
+            return vmMedicines;
+        }
+
+        public MedicineAlternativeViewModel ConvertMedicineToAlternativeViewModel(Medicine medicine)
+        {
+            MedicineAlternativeViewModel medicineViewModel = new MedicineAlternativeViewModel
+            {
+                Id = medicine.Id,
+                Name = medicine.Name,
+                _Medicine = medicine
+            };
+            return medicineViewModel;
+        }
+
+        public ObservableCollection<MedicineAlternativeViewModel> ConvertCollectionToAlternativeViewModel(List<Medicine> medicines)
+        {
+            ObservableCollection<MedicineAlternativeViewModel> vmMedicines = new ObservableCollection<MedicineAlternativeViewModel>();
+            MedicineAlternativeViewModel medicineViewModel;
+            foreach (Medicine medicine in medicines)
+            {
+                medicineViewModel = ConvertMedicineToAlternativeViewModel(medicine);
+                vmMedicines.Add(medicineViewModel);
+            }
+            return vmMedicines;
+        }
+    }
+}
